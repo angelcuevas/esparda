44830fc162d37a38200205f4c45babe2ee9fb34e
PabloRios <PabloRios@users.noreply.github.com>
2015-09-30 01:26:05 -0300

    Logear distancia total 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -11,10 +11,10 @@ function initMap(){
         [],
         //Waypoints Linea 1
         [
-            {lat: -29.40964546680778, lng: -66.9158148765564},
-            {location: new google.maps.LatLng(-29.411664231008626, -66.9158148765564), stopover: false},
-            {location: new google.maps.LatLng(-29.411664231008626, -66.9158148765564), stopover: false},
-            {lat: -29.412365181422135, lng: -66.91378712654114}
+            {lat: -29.40964546680778, lng: -66.9158148765564}, //Inicio de linea 1
+            {location: new google.maps.LatLng(-29.411664231008626, -66.9158148765564), stopover: false}, //Punto 1
+            {location: new google.maps.LatLng(-29.41433716251066, -66.91343307495117), stopover: false},
+            {lat: -29.413839965334418, lng: -66.90354108810425} //Fin de linea 1
         ]
     ];
 
@@ -26,9 +26,9 @@ function initMap(){
     });
     directionsDisplay.setMap(map);
 
-
+    //TODO: switch case que seleccione el array dependiendo de la linea seleccionada
     directionsService.route({
-    origin: waypoints[1].shift(),
+    origin: waypoints[1].shift(), //Primer objeto del arreglo
     destination: waypoints[1].pop(), //Último objeto del arreglo
     waypoints: waypoints[1],
     optimizeWaypoints: true,
@@ -37,12 +37,13 @@ function initMap(){
         if (status === google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
             var route = response.routes[0];
+            for (var i = 0; i < route.legs.length; i++) {
+                console.log("Inicio: " +route.legs[i].start_address + " Fin:" + route.legs[i].end_address + " Distancia: " + route.legs[i].distance.text);
+            }
         } else {
           window.alert('Directions request failed due to ' + status);
         }
     });
-
-
 
     //Obtener Latitud y longitud al hacer click
     google.maps.event.addListener(map, 'click', function(event) {
